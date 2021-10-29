2e15dc09215bfd25e93f63df3b55648179b1697a
ChuanXu1 <cx1@sanger.ac.uk>
2021-10-29 18:10:50 +0100

    Update JSON and script

--- src/generate_json_from_latest_models.py
+++ src/generate_json_from_latest_models.py
@@ -1,34 +1,43 @@
 from celltypist import models
 import json
 from datetime import datetime
+import os
+import sys
 
-#download latest models
-models.download_models(force_update = True)
-model_folder = models.models_path
-print(f"models are stored in {model_folder}")
+##Before all of these...
+# 1) All models in the s3 are up-to-date, some of them will be usable by the users, some for internal use
+# 2) Store all usable (see 1 above) models in `model_folder` 
+# 3) 1) and 2) should both be up-to-date
+
+#all usable models
+model_folder = sys.argv[1]
+all_models = os.listdir(model_folder)
+all_models = sorted([x for x in all_models if x.startswith('Immune')]) + sorted([x for x in all_models if not x.startswith('Immune')])
 
 #basic info
 dict_ = {"description": "CellTypist model list", "last_update": str(datetime.now())}
 
 #model list
 model_list = []
-all_models = models.get_all_models()
 for each_model in all_models:
-    model_load = models.Model.load(each_model)
-    #
+    model_load = models.Model.load(f"{model_folder}/{each_model}")
     filename = each_model
     url = model_load.description['url']
-    version = url[43:45]
+    #<-----------------------------Add version from here----------------------------->
+    if filename.startswith('Immune'):
+        version = url[43:45]
+    elif (filename == 'Cells_Lung_Airway.pkl') or (filename == 'Nuclei_Lung_Airway.pkl'):
+        version = 'v1'
+    else:
+        version = 'v1'
+    #<-----------------------------Add version end here----------------------------->
     date = model_load.description['date']
     details = model_load.description['details']
     number_celltypes = model_load.description['number_celltypes']
-    #
     each_info = {"filename": filename, "url": url, "version": version, "date": date, "details": details, "No_celltypes": number_celltypes}
     if filename == 'Immune_All_Low.pkl':
         each_info['default'] = True
     model_list.append(each_info)
-
-#combine info
 dict_["models"] = model_list
 
 #dump
