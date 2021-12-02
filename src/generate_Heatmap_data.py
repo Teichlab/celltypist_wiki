e3ba120087a84045ad792df3357547f38ea37f97
ChuanXu1 <cx1@sanger.ac.uk>
2021-12-02 11:23:41 +0000

    Add tissue-order file

--- src/generate_Heatmap_data.py
+++ src/generate_Heatmap_data.py
@@ -37,7 +37,7 @@ def celltypist_AverageExpression_PercentExpression(adata,
 adata = sc.read(sys.argv[1])
 #sc.pp.normalize_total(adata, target_sum=1e4)
 #sc.pp.log1p(adata)
-tissue_order = ["Skin", "Tonsil", "Liver", "Placenta", "Decidua", "Lung", "Upper airway", "Trachea", "Thymus", "Eye", "Colon", "Mesenteric lymph node", "Intestine", "Oesophagus", "Kidney", "Spleen", "Omentum adipose tissue", "Lung-draining lymph node", "Blood", "Bone marrow"]
+tissue_order = pd.read_csv('Heatmap_data/tissue_order.txt', header = None)[0].values
 celltype_order = pd.read_csv('Heatmap_data/celltype_order.txt', header = None)[0].values
 exp_pct = celltypist_AverageExpression_PercentExpression(adata, 'Tissue', 're_harmonise_annotation', tissue_order, celltype_order, False, 10)
 with open('Heatmap_data/exp_pct_celltypist_immune.pkl', 'wb') as f:
