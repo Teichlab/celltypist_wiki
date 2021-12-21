c827172ad32f85b297813cb69c7af7a70b70c902
ChuanXu1 <cx1@sanger.ac.uk>
2021-12-21 22:42:10 +0000

    Modify heatmap script for multi-atlases

--- src/generate_Heatmap_data.py
+++ src/generate_Heatmap_data.py
@@ -5,15 +5,26 @@ import sys
 import scanpy as sc
 import os
 ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+atlas = sys.argv[1]
+#setting->
+setting = pd.read_csv(f"{ROOT_PATH}/atlases/{atlas}/config/Heatmap.config", header = None, index_col = 0)
+adata = sc.read(setting.loc['adata_path', 1])
+tissue_column = setting.loc['tissue_column', 1]
+celltype_column = setting.loc['celltype_column', 1]
+tissue_order = pd.read_csv(f'{ROOT_PATH}/atlases/{atlas}/Heatmap_data/tissue_order.txt', header = None)[0].values
+celltype_order = pd.read_csv(f'{ROOT_PATH}/atlases/{atlas}/Heatmap_data/celltype_order.txt', header = None)[0].values
+use_raw = setting.loc['use_raw', 1]
+filter_out = int(setting.loc['filter_out', 1])
+do_normalize = setting.loc['do_normalize', 1]
+#<-setting
 
 def celltypist_AverageExpression_PercentExpression(adata,
                                                    group1_by, group2_by,
                                                    group1_order, group2_order,
-                                                   use_raw = True,
+                                                   use_raw = 'True',
                                                    filter_out = 5):
     #group1 == organ ;  group2 == cell type
-    adata = adata.copy()
-    adata = adata.raw.to_adata() if use_raw else adata
+    adata = adata.raw.to_adata() if use_raw in ['true', 'True', 'TRUE'] else adata
     adata.X = np.expm1(adata.X)
     #assert groups
     assert np.all(np.unique(adata.obs[group1_by]) == np.unique(np.array(group1_order)))
@@ -36,17 +47,9 @@ def celltypist_AverageExpression_PercentExpression(adata,
     return {'exp': np.log1p(exp), 'pct': pct, 'group1': group1_order, 'group2': group2_order}
 
 #main
-adata = sc.read(sys.argv[1])
-#sc.pp.normalize_total(adata, target_sum=1e4)
-#sc.pp.log1p(adata)
-#-----------------------> setting here
-tissue_column = 'Tissue'
-celltype_column = 're_harmonise_annotation'
-tissue_order = pd.read_csv(f'{ROOT_PATH}/Heatmap_data/tissue_order.txt', header = None)[0].values
-celltype_order = pd.read_csv(f'{ROOT_PATH}/Heatmap_data/celltype_order.txt', header = None)[0].values
-use_raw = False
-filter_out = 10
-#<----------------------- setting here
+if do_normalize in ['true', 'True', 'TRUE']:
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
 exp_pct = celltypist_AverageExpression_PercentExpression(adata, tissue_column, celltype_column, tissue_order, celltype_order, use_raw, filter_out)
-with open(f'{ROOT_PATH}/Heatmap_data/exp_pct_celltypist_immune.pkl', 'wb') as f:
+with open(f'{ROOT_PATH}/atlases/{atlas}/Heatmap_data/exp_pct_celltypist.pkl', 'wb') as f:
     pickle.dump(exp_pct, f)
